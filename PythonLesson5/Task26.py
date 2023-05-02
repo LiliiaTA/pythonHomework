0fa7b4fc70fc38c3f105cfe248f7ef0d116569ae
Liliia Albertovna <122629161+LiliiaTA@users.noreply.github.com>
2023-05-02 09:29:14 +0500

    Add files via upload

---
+++ PythonLesson5/Task26.py
@@ -0,0 +1,17 @@
+# Задача 26:  Напишите программу, которая на вход принимает два числа A и B, 
+# и возводит число А в целую степень B с помощью рекурсии.
+
+def power(a, b):
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a
+    elif b % 2 == 0:
+        return power(a * a, b // 2)
+    else:
+        return a * power(a, b - 1)
+
+a = int(input("Введите число A: "))
+b = int(input("Введите степень B: "))
+
+print(f"{a}^{b} = {power(a, b)}")
